efbd984864fb53663adf8aaaae34d8c8cc182eb9
EmreSani <emresani1998@gmail.com>
2024-09-04 15:11:45 +0300

    first commit

---
+++ src/main/java/com/dev02/TimeTrackingApp/TimeTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev02.TimeTrackingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackingAppApplication.class, args);
+	}
+
+}
