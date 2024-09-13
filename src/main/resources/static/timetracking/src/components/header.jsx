ddda51a407f082c4751293ed173cd5de4b70b3d9
Osman Şeyban <osmanseyban@gmail.com>
2024-09-13 15:38:09 +0300

    frontend started

---
+++ src/main/resources/static/timetracking/src/components/header.jsx
@@ -0,0 +1,12 @@
+// src/components/header.jsx
+import React from 'react';
+import '../App.css';
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>Time Tracking Dashboard</h1>
+    </header>
+  );
+};
+
